974ea98ee5aebdc2f4845b177c0f207f618d8a60
seplitza <seplitza@gmail.com>
2026-01-02 11:04:52 +0300

    Fix: Add request and endpoints imports

--- web/src/pages/courses.tsx
+++ web/src/pages/courses.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { request, endpoints } from '../api';
 import {
   fetchMyOrders,
   fetchAvailableCourses,
